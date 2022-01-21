01f1ff1ac05fa77eb96394d1bc13d8ad72152744
Alex Wickstrom <awickstrom@ucla.edu>
2022-01-21 09:38:31 -0800

    first commit

---
+++ homepage/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# # Create your models here.
+# class PaidUser(AbstractUser):
+#     uname = models.CharField(max_length=200)
+#     related_name = "paid_user"
